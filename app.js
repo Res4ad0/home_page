28af390d72ffeccabf20c9bd13224ae180f004a3
Reshad Rustemov <142893459+Res4ad0@users.noreply.github.com>
2024-07-20 19:14:41 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+document.getElementById('quote-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Your quote request has been submitted!');
+});
+
+
+document.getElementById('subscribe-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('You have subscribed successfully!');
+});
